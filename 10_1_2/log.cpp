9e279e5fa89d24594f79e7140983765acdbb4928
Ville Inkinen <ville.inkinen@brightstraining.com>
2023-08-15 09:42:16 +0300

    10_1_2

--- 10_1_2/log.cpp
+++ 10_1_2/log.cpp
@@ -1,3 +1,4 @@
+#include <atomic>
 #include <chrono>
 #include <iostream>
 #include <mutex>
@@ -14,7 +15,7 @@ void print_logs(const log_vec &logs) noexcept
         std::cout << log << "\n";
     }
 }
-void read_inputs(log_vec &logs, bool &terminate)
+void read_inputs(log_vec &logs, std::atomic_bool &terminate)
 {
     std::lock_guard<std::mutex> log_lock(log_mutex);
     std::string user_input;
@@ -30,7 +31,7 @@ void read_inputs(log_vec &logs, bool &terminate)
     }
     terminate = !terminate; // flip terminate to exit peacefully
 }
-void log_timer(log_vec &logs, bool &terminate)
+void log_timer(log_vec &logs, std::atomic_bool &terminate)
 {
     while (!terminate)
     {
@@ -42,7 +43,7 @@ int main()
 {
     log_vec logs;
 
-    bool terminate = false; // for the log_timer
+    std::atomic_bool terminate{false}; // for the log_timer
 
     std::thread timer_thread(log_timer, std::ref(logs), std::ref(terminate));
     std::thread log_thread(read_inputs, std::ref(logs), std::ref(terminate));
